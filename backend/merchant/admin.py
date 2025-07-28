dc1203b07798e60e70b0a7a9713cf4c650f3b2e6
AashishBasnet <basnetashish756@gmail.com>
2025-07-28 12:54:13 +0545

    removed orderitem

--- backend/merchant/admin.py
+++ backend/merchant/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from .models import Merchant, Restaurant, FoodItem, Order
+from .models import Merchant, Restaurant, FoodItem, Order, Delivery, Deliveryman, Cuisine
 # Register your models here.
 
 admin.site.register(Merchant)
 admin.site.register(Restaurant)
 admin.site.register(FoodItem)
 admin.site.register(Order)
-# admin.site.register(OrderItem)
-# admin.site.register(Delivery)
-# admin.site.register(Deliveryman)
-# admin.site.register(Cuisine)
+admin.site.register(Delivery)
+admin.site.register(Deliveryman)
+admin.site.register(Cuisine)
