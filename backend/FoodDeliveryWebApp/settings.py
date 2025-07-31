5d96e68edecd6aa02ac01f532349b0a3b32615f1
SandeshPChhetri <chhetrisameer987@gmail.com>
2025-07-31 16:39:16 +0545

    setting bug fixed

--- backend/FoodDeliveryWebApp/settings.py
+++ backend/FoodDeliveryWebApp/settings.py
@@ -162,7 +162,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWS_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
 
 JAZZMIN_SETTINGS = {
     "site_title": "GrubMate Admin",
